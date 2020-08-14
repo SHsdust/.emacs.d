1c76fe9779232335e3650be77031728d298e88cd
emrys <s.hsdust@outlook.com>
2020-08-14 17:53:36 +0800

    avoid PATH warning

--- lisp/init-exec-path.el
+++ lisp/init-exec-path.el
@@ -8,6 +8,8 @@
   (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE" "NIX_SSL_CERT_FILE" "NIX_PATH"))
     (add-to-list 'exec-path-from-shell-variables var)))
 
+;; avoid annoying PATH warning
+(setq exec-path-from-shell-arguments nil)
 
 (when (or (memq window-system '(mac ns x))
           (unless (memq system-type '(ms-dos windows-nt))
